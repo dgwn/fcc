99464abc4ecb5d662d9a15970b581ab116d00ce5
Daniel Gawne <danielgawne@gmail.com>
2019-07-23 14:13:05 -0400

    add howManyNumbers()

--- js_algorithms/telephone_number_validator.js
+++ js_algorithms/telephone_number_validator.js
@@ -1,7 +1,16 @@
 function telephoneCheck(str) {
 
   // determine how many numbers are in the string
-
+  function howManyNumbers(){
+    let split = str.split('');
+    let numOfNum = 0;
+    for (let i = 0; i < split.length; i++) {
+      if (!isNaN(split[i])) {
+        numOfNum++;
+      }
+     }
+     console.log(numOfNum)
+  }
 
   // check if there are either 10 or 11 numbers in the test
     // if no, false
