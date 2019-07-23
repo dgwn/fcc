ddadb96187d5ebe7f30cfc4dc1dcc4e6b7bc22db
Daniel Gawne <danielgawne@gmail.com>
2019-07-23 14:12:38 -0400

    add telephone number validator alg

---
+++ js_algorithms/telephone_number_validator.js
@@ -0,0 +1,18 @@
+function telephoneCheck(str) {
+
+  // determine how many numbers are in the string
+
+
+  // check if there are either 10 or 11 numbers in the test
+    // if no, false
+    // if yes and there are 11 numbers, check if first number is 1
+      // if no, false
+      // if yes, run checkFormat()
+
+    // checkFormat()
+
+
+  howManyNumbers(
+}
+
+telephoneCheck("555-555-5555");
