b10dc1cbf948e7289b79ef6e38f35f7e5ed3ba85
Daniel Gawne <danielgawne@gmail.com>
2019-08-05 15:20:19 -0400

    add checkFormat() to finish project

--- js_algorithms/telephone_number_validator.js
+++ js_algorithms/telephone_number_validator.js
@@ -14,22 +14,65 @@ function telephoneCheck(str) {
 
   // check if there are either 10 or 11 numbers in the test
   if (howManyNumbers() === 10) {
-    return true;
+    return checkFormat();
   }
+  // if there are 11 numbers, make sure the country code is 1
   else if (howManyNumbers() === 11 && parseInt(str.split('')[0], 10) === 1) {
-    return true;
+    return checkFormat();
   }
   else return false;
 
-    // if no, false
-    // if yes and there are 11 numbers, check if first number is 1
-      // if no, false
-      // if yes, run checkFormat()
-
     // checkFormat()
+      // check parenth
+        // if it contains parentheses, only return true if they are after one (and close 3 places later) or if they begin the num and close 3 places later
 
+  // check whether parentheses have proper closure
+  function checkFormat() {
+    let split = str.split('');
+    // check whether there are no parentheses
+    if (!str.includes(")") && !str.includes("(")) {
+      console.log("no parenthesis");
+      return true;
+    }
+    // check whether there is an openening parenthesis
+    else if (str.includes("(") && str.includes(")")) {
+      // check whether the opening parenthesis is an an appropriate spot
+      if (split.indexOf("(") === 0 || split.indexOf("(") === 1 || split.indexOf("(") === 2) {
+        // check whether the corresponding closing parenethesis
+        if (split.indexOf(")") === (split.indexOf("(") + 4)) {
+          // check whether there are extra opening parenthesis
+          let opening = 0
+          for (let i=0; i < split.length; i++) {
+            if (split[i] == "(") {
+             opening++;
+            }
+          }
+          if (opening === 1) {
+            return true
+          }
+          else return false;
+        }
+        else {
+          return false;
+        }
+      }
+      else {
+        return false;
+      }
+    }
+    // check whether opening parenthesis is missing
+    else if (str.includes(")") && !str.includes("(")) {
+      return false;
+    }
+    // check whether closing parenthesis is missing while open is present
+    else if (str.includes("(") && !str.includes(")")) {
+      return false;
+    }
+    // all other cases
+    else return false;
 
-  howManyNumbers();
+  }
+
 }
 
 telephoneCheck("555-555-5555");
